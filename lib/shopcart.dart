614a002d2346c4380a04a358ea4d1e9b4f69162e
Hibrizy Sulistiyo <hibrizys@gmail.com>
2023-05-04 21:11:55 +0700

    tes dulu

--- lib/shopcart.dart
+++ lib/shopcart.dart
@@ -70,7 +70,7 @@ class _shopcartState extends State<shopcart> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "Anorak Jacket",
+                          "Anorak Jackett",
                           style: TextStyle(
                               fontWeight: FontWeight.bold, fontSize: 20),
                         ),
